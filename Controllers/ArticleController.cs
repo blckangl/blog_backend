10f7b2ecb44db183cfa872f6ab3d4a787998a829
dhia <dhiaa.kahri@gmail.com>
2020-11-08 16:54:48 +0100

    first init

---
+++ Controllers/ArticleController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using blog.Models;
+using blog.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace blog.Controllers
+{
+    
+    [ApiController]
+    [Route("api/articles")]
+    public class ArticleController : ControllerBase
+    {
+        private readonly ArticleService _articleService;
+        public ArticleController(ArticleService articleService)
+        {
+            _articleService = articleService;
+        }
+        
+        [HttpGet]
+        public List<Article> getAll()
+        {
+            return _articleService.getAllArticles();
+        }
+
+        [HttpPut("{id}")]
+        public Article UpdateArticle(long id, Article art)
+        {
+           return _articleService.UpdateArticle(id, art);
+        }
+
+        [HttpGet("{id}")]
+        public Article getArticleById(long id)
+        {
+            return _articleService.GetArticleByd(id);
+        }
+
+        [HttpDelete("{id}")]
+        public bool DeleteArticle(long id)
+        {
+            return _articleService.DeleteArticle(id);
+        }
+    }
+}
---
+++ Models/Article.cs
@@ -0,0 +1,10 @@
+﻿namespace blog.Models
+{
+    public class Article
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Text { get; set; }
+        public string Image { get; set; }
+    }
+}
